ff4e6174b711756bc32787bff72af15539a5de68
Aufa Billah <aufaroot18@gmail.com>
2021-03-16 16:52:02 +0700

    Add Pertemuan 3: Code & Slide

---
+++ Pertemuan 3/Code/echo.sh
@@ -0,0 +1,10 @@
+# Mencetak teks
+echo "Hai apa kabar?"
+
+# Mencetak variabel
+nama="Aufa Billah"
+echo "Nama saya $nama";
+
+echo "Saya sedang berada di: $(pwd)"
+
+echo "Hari ini tanggal: $(date)"
---
+++ Pertemuan 3/Code/read.sh
@@ -0,0 +1,16 @@
+# Mengambil inputan user
+echo -n "Masukan nama Anda: "
+read nama
+
+echo -n "Masukan umur Anda: "
+read umur
+
+echo -n "Masukan alamat Anda: "
+read alamat
+
+# Menampilkan variabel nama
+echo "Hai $nama, Terima kasih sudah mengisi data"
+echo "Data Anda sudah disimpan"
+echo "Nama: $nama"
+echo "Umur: $umur tahun"
+echo "Alamat: $alamat"
